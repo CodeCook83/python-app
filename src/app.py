390ac96fe1f6ac41d547ed61bd8e9a4afb286b7c
CodeCook <codecook@posteo.de>
2026-02-18 11:27:33 +0100

    ci job fix 2

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'Hey Boy :-)',
+        'message': 'Hey Boy',
         'deployed_on': 'kubernetes'
     })
 
